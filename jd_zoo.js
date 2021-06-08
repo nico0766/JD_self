3c409fd6121019f95cd3030c7c4aa3432dd2ad4c
nico0766 <58452790+nico0766@users.noreply.github.com>
2021-06-08 23:21:59 +0800

    Update jd_zoo.js

--- jd_zoo.js
+++ jd_zoo.js
@@ -12,6 +12,20 @@ PK互助：内部账号自行互助(排名靠前账号得到的机会多),多余
 脚本更新时间：2021-06-08 19:00
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 ===================quantumultx================
+[task_local]
+#618动物联萌
+33 0,6-23/2 * * * https://gitee.com/lxk0301/jd_scripts/raw/master/jd_zoo.js, tag=618动物联萌, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+
+=====================Loon================
+[Script]
+cron "33 0,6-23/2 * * *" script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_zoo.js, tag=618动物联萌
+
+====================Surge================
+618动物联萌 = type=cron,cronexp="33 0,6-23/2 * * *",wake-system=1,timeout=3600,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_zoo.js
+
+============小火箭=========
+618动物联萌 = type=cron,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_zoo.js, cronexpr="33 0,6-23/2 * * *", timeout=3600, enable=true
+ */
 const $ = new Env('618动物联萌');
 const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
